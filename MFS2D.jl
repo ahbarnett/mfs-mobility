7dd5bb1fbac2f41312653dfa511cf4a0f8c6e17c
ahbarnett <alex.h.barnett@gmail.com>
2023-11-03 20:57:29 -0400

    seeing if vscode reports git status

--- MFS2D.jl
+++ MFS2D.jl
@@ -1,7 +1,7 @@
 module MFS2D
-export lapchgmat, lapchgeval
+export lapchgpotmat, lapchgeval
 """
-A = lapchgmat(t, s)
+A = lapchgpotmat(t, s)
 
 Fill dense MFS matrix for 2D Laplace fundamental solution, plain
 charges to potentials.
@@ -10,7 +10,7 @@ Kernel is (1/2pi) log(1/r). No augmentation by any const term.
 t is M*2 target coords, s is N*2 source coords.
 A is M*N
 """
-function lapchgmat(t, s)
+function lapchgpotmat(t, s)
     N = size(s,1)
     M = size(t,1)
 	@assert size(s,2)==2  # check dim
@@ -21,21 +21,21 @@ function lapchgmat(t, s)
         for j = 1:N
             d1 = s[j,1] - t[i,1]
             d2 = s[j,2] - t[i,2]
-            r2 = d1*d1 + d2*d2    # squared dist
-            A[i,j] = (-1/4pi) * log(r2)
+            rr = d1*d1 + d2*d2    # squared dist
+            A[i,j] = (-1/4pi) * log(rr)
         end
     end
 	A
 end
 
 """
-u = lapchgeval(t, s, co)
+u,gradu = lapchgeval(t, s, co)
 
 Direct summation of 2D Laplace fundamental solutions, charges to potentials
-Kernel is (1/2pi) log(1/r). No const term.
+and gradients. Kernel is (1/2pi) log(1/r). No const term.
 
 t is M*2 target coords, s is N*2 source coords, co is (N,) coeff vec
-Output u is (M,)
+Outputs: u is (M,), gradu is (M,2)
 """
 function lapchgeval(t, s, co)
     N = size(s,1)
@@ -43,16 +43,21 @@ function lapchgeval(t, s, co)
 	@assert size(s,2)==2  # check dim
 	@assert size(t,2)==2
     u = zeros(M)   # assume Float64
+	gradu = zeros(M,2)
     for i = 1:M
         for j = 1:N
             d1 = s[j,1] - t[i,1]
             d2 = s[j,2] - t[i,2]
-            r2 = d1*d1 + d2*d2
-            u[i] += co[j] * log(r2)
+            rr = d1*d1 + d2*d2
+            u[i] += co[j]*log(rr)
+			tmp = co[j]/rr
+			gradu[i,1] += d1*tmp
+			gradu[i,2] += d2*tmp
         end
 		u[i] *= -1/4pi
+		gradu[i,:] *= -1/2pi
     end
-	u
+	u, gradu
 end
 
 end  # module
--- test.jl
+++ test.jl
@@ -16,24 +16,27 @@ s,_,_ = unitcircle(N)    # source pts
 s *= 0.7
 
 @printf "1-body exterior Dirichlet BVP with specified net charge Sig...\n"
-A=lapchgmat(t,s)
+# this serves to test MFS2D module
+A=lapchgpotmat(t,s)
 A = [A; ones(1,N)]    # extra condition on net charge
 A = [A [ones(M,); 0.0] ]   # append overall const as a dof (can't tie it to net chg)
 println("N=$N, M=$M, last sing vals=",svd(A).S[end-2:end])
 s0 = [1.1 0.8]        # incident source pt (row vec), not too close 
-uinc = lapchgmat(t,s0)[:]   # unit chg,  [:] makes a col vec not M*1 matrix
+uinc = lapchgpotmat(t,s0)[:]   # unit chg,  [:] makes a col vec not M*1 matrix
 Sig = 1.7          # desired net charge from this body (needn't match that of inc src)
 rhs = [-uinc; Sig]
 co = A\rhs
 r=norm(A*co-rhs)/norm(rhs)
-ut = lapchgeval(t,s,co[1:N]) .+ co[end]    # eval
+ut,gradut = lapchgeval(t,s,co[1:N])    # mfs eval on surf
+ut .+= co[end]    # add in const term
 re = norm(ut+uinc)/norm(uinc)              # rel surf error 
 @printf "solved\trelresid=%.3g, bdryrelerr=%.3g, norm(c)=%.3g\n" r re norm(co)
-@printf "\ttot charge err=%.3g\n" sum(co[1:N])-Sig
+fluxt = dot(tw,sum(gradut.*tn,dims=2))     # surf integral of u_n
+@printf "\ttot charge err=%.3g (via co), %.3g (u_n)\n" sum(co[1:N])-Sig fluxt-Sig
 if verb>0       # plot soln and geom
     ng=300; g = range(-2,2,ng); o=ones(size(g)); gg=[kron(o,g) kron(g,o)]
-    ugg = lapchgeval(gg,s,co[1:N]) .+ co[end]   # u_scatt on grid
-    ugg += lapchgmat(gg,s0)[:]              # add in u_inc
+    ugg,_ = lapchgeval(gg,s,co[1:N]); ugg .+= co[end]   # u_scatt on grid
+    ugg += lapchgpotmat(gg,s0)[:]              # add in u_inc
     fig,ax,h = heatmap(g,g,reshape(ugg,ng,ng), axis=(aspect=DataAspect(),))
     h.colorrange=0.2*[-1,1]; h.colormap=:jet
     scatter!(s[:,1],s[:,2],color=:red, label="proxy src")
