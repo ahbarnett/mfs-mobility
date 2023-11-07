5258000de428992cb81a9d471eb8214146f50e8d
ahbarnett <alex.h.barnett@gmail.com>
2023-11-06 21:45:39 -0500

    utils with loading sphdesign files

--- showspherepts.jl
+++ showspherepts.jl
@@ -10,22 +10,24 @@ Ns = getavailablepoints()
 N = Ns[20]   # 1202 pts
 x,y,z,w = lebedev_by_points(N)
 X = [x y z]
-#maximum(w)/minimum(w)     large dyn range (~8), small at oct corners
-#GLMakie.activate!(title="Lebedev pts")
-#fig,ax,l = scatter(x,y,z,color=w)
-#l.colormap=:jet; Colorbar(fig[1,2],l)
-#ig
+println("max to min weight ratio: ",maximum(w)/minimum(w))
+# large dyn range (~8), small at octahedron corners
+GLMakie.activate!(title="Lebedev pts")
+fig,ax,l = scatter(X[:,1],X[:,2],X[:,3],color=w)
+l.colormap=:jet; Colorbar(fig[1,2],l)
+display(fig)            # to see fig in vscode, since not last
 sum(w)   # 1
-w .*= 4pi   # make surf element
+w .*= 4pi   # make weights wrt surf element
 println(sum(w))
 
 # try spherical designs (2/3 Gaussian efficiency wrt degree, but quasi-unif)
+using DelimitedFiles
 Xd = readdlm("sphdesigns/sf048.01202")  # same N as above
 Nd = size(Xd,1)
-wd = ones(Nd)*4pi/Nd
-println(sum(wd))
+wd = ones(Nd)*4pi/Nd    # make own weights
+println(sum(wd))        # exact
 GLMakie.activate!(title="Wormseley sph design pts")
-fig,ax,l = scatter(Xd[:,1],Xd[:,2],Xd[:,3],color=wd)
-l.colormap=:jet; Colorbar(fig[1,2],l)
+fig,ax,l = scatter(Xd[:,1],Xd[:,2],Xd[:,3],color=:black,size=1)
 display(GLMakie.Screen(), fig)           # new figure
+#GLMakie.closeall()
 
--- utils.jl
+++ utils.jl
@@ -1,3 +1,5 @@
+using DelimitedFiles         # for text file reading
+
 """
 x,w,nx = unitcircle(N::Integer)
     
@@ -32,3 +34,43 @@ function starfish(N::Integer=100,freq::Integer=5,ampl=0.3,rot=1.0)
     w = (2*pi/N)*sp                   # PTR
     x,w
 end
+
+"""
+    Ns = getavailablesphdesigns()
+
+returns available list of numbers of spherical design points.
+`Ns[t]` is the number of points in the spherical design exact for
+degree `t`, for t=1,...,180.
+
+Also see: sphdesign_by_points
+"""
+function getavailablesphdesigns()
+    # file list with leading "sf" chars removed (dot is separator)...
+    tN=readdlm(string(@__DIR__,"/sphdesigns/filelist.txt"),'.',Int)
+    # (note linux, OSX, not Windows)
+    tN[:,2]
+end
+
+"""
+    X, w = sphdesign_by_points(Nmax)
+
+Returns the largest spherical design (quadrature points on S^2) with
+no more than `Nmax` points. `X` is the (N,3) coordinate array in R^3
+of the N points, and `w`, a (N,) column vector of their corresponding
+weights. (The weights are equal, thus easily calculated by the user anyway.)
+N<=Nmax, with N also <= 180, the largest design available.
+
+Also see: [`getavailablesphdesigns`](@ref)
+"""
+function sphdesign_by_points(Nmax::Integer=1202)
+    Ns = getavailablesphdesigns()
+    t = findlast(N -> N<=Nmax, Ns)        # degree
+    @assert !isnothing(t) "no possible N match your Nmax!"
+    N = Ns[t]     # the largest N not exceeding Nmax
+    fnam = @sprintf "sf%.3d.%.5d" t N     # reverse-engineer filename
+    absfnam = string(@__DIR__,"/sphdesigns/",fnam)
+    X = readdlm(absfnam)
+    @assert size(X,1)==N "read wrong number of lines from file!"
+    w = ones(N)*(4pi/N)
+    X,w
+end 
