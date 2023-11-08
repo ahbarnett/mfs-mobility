4edbf9e868119fd7e614dadca28ce813d60c9676
ahbarnett <alex.h.barnett@gmail.com>
2023-11-07 22:31:22 -0500

    3d 1-sph MFS Dir ext conv pic

--- test3ddir.jl
+++ test3ddir.jl
@@ -7,13 +7,16 @@ using LinearAlgebra
 using GLMakie
 using Printf
 
+verb = 1
 x0 = [1.1,0.8,-0.4]'      # u inc src point far from unit sphere
 R = 0.7                   # MFS source radius
-@printf "1-body ext Lap Dir 3D conv (scatt BVP, src dist %.3g, R=%g)...\n" norm(x0) R
-sph_pts = get_sphdesign   # which type of surf discr
-Nas = 500:500:2000        # conv study: upper limits for N
-Mratio = 1.2              # desired M/N
+if verb>0 fig = Figure(); ax = Axis(fig[1,1], yscale=log10, xlabel=L"N", ylabel="rel surf err"); end
+for sph_pts = [get_sphdesign, get_lebedev]   # which type of surf discr
+Nas = 300:300:2000        # conv study: upper limits for N
+Mratio = 1.5              # desired M/N
+@printf "1-body ext Lap Dir 3D conv (scatt BVP, src dist %.3g, R=%g, %s)...\n" norm(x0) R string(sph_pts)
 Ns = similar(Nas)
+es = zeros(size(Ns))      # record errors
 for (i,Na) in enumerate(Nas)
     Y,_ = sph_pts(Na)
     Y *= R
@@ -26,8 +29,14 @@ for (i,Na) in enumerate(Nas)
     co = A\rhs
     r=norm(A*co-rhs)/norm(rhs)
     ut,gradut = lap3dchgeval(X,Y,co)      # MFS eval on surf
-    re = norm(ut+uinc)/norm(uinc)         # rel surf pot error 
+    re = es[i] = norm(ut+uinc)/norm(uinc) # rel surf pot error 
     @printf "N=%d M=%d relresid=%.3g bdryrelerr=%.3g norm(c)=%.3g\n" N M r re norm(co)
     flux = dot(w,sum(gradut.*X,dims=2))   # surf int u_n (uinc_n flux=0)
-    @printf "\ttot chg=%.8g, u_n flux=%.8g\n" sum(co) flux
+    @printf "\ttot chg=%.12g (err vs u_n flux %.3g)\n" sum(co) sum(co)-flux
 end
+#fig,ax,l = scatterlines(Ns,es, axis = (yscale=log10,), xlabel=L"N", ylabel="rel surf err")
+if verb>0 scatterlines!(Ns,es, label=string(sph_pts)) end
+end
+if verb>0 axislegend(); ax.title="ext Dir Lap 3D sph MFS conv"; display(fig); end
+
+
